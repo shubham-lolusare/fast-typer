1eb59605c9a04ac72d824d96368b8ba6b3ca2a33
Shubham Lolusare <shubhamlolusare@gmail.com>
2023-05-27 09:42:27 +0530

    router check

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Page1 from "./page1.jsx";
+import Page2 from "./page2.jsx";
+import Error from "./error.jsx";
+import "./index.css";
+import {
+  createHashRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createHashRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<Error />}>
+      <Route index element={<Page1 />} />
+      <Route path="page2" element={<Page2 />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/page1.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function Page1() {
+  return (
+    <div>
+      page1
+      <Link to="page2">to page2</Link>
+    </div>
+  );
+}
