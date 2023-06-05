64be261b9ab17b03485f0e045a4dc5051cbda650
Shubham Lolusare <shubhamlolusare@gmail.com>
2023-06-05 18:53:36 +0530

    content added

---
+++ src/components/TypingTestPage/TypingResultModal.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/prop-types */
+import { RxCross1 } from "react-icons/rx";
+import randomWords from "random-words";
+
+export default function TypingResultModal({
+  correctWordCount,
+  setCorrectWordCount,
+  correctCharCount,
+  setCorrectCharCount,
+  attemptedWords,
+  setWordIndex,
+  setCharIndex,
+  setTestState,
+  setShowResultModal,
+  setWords,
+}) {
+  function handleReset() {
+    setCorrectWordCount(0);
+    setCorrectCharCount(0);
+    setCharIndex(0);
+    setWordIndex(0);
+    setTestState("");
+    setShowResultModal(false);
+    setWords(randomWords({ min: 300, max: 1000 }));
+  }
+
+  return (
+    <div className="h-screen w-full absolute top-0 left-0 bg-black/90 flex justify-center items-center">
+      <div className="bg-bgColor relative w-[40%] h-[55%] rounded-xl p-4 text-textColor flex flex-col gap-4 justify-between">
+        <RxCross1
+          className="absolute right-6 top-8 hover:text-thematicColor"
+          onClick={handleReset}
+        />
+        <div className="h-full flex flex-col gap-4">
+          <h1 className="text-textColor text-4xl font-bold">
+            Your Test Result
+          </h1>
+          <div className="flex-1 flex flex-col gap-8">
+            <div className="text-6xl">Excellent</div>
+
+            <div className="flex-1 flex gap-4">
+              <div className="p-4 bg-thematicColor flex-1 rounded-xl shadow-lg flex flex-col justify-evenly items-center">
+                <div className="text-9xl font-bold">{correctWordCount}</div>
+                <div>Words per minute</div>
+              </div>
+              <div className="p-4 bg-thematicColor flex-1 rounded-xl shadow-lg flex flex-col justify-between items-start">
+                <div className="text-5xl font-bold">{correctCharCount} CPM</div>
+                <div className="text-sm">with</div>
+                <div className="text-6xl">
+                  {correctWordCount != 0
+                    ? Math.round((correctWordCount / attemptedWords) * 100)
+                    : 0}
+                  % accuracy
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/TypingTestPage/TypingTestPage.jsx
+++ src/components/TypingTestPage/TypingTestPage.jsx
@@ -2,13 +2,13 @@ import Navbar from "../Navbar/Navbar";
 import randomWords from "random-words";
 import "./TypingTestPage.css";
 import Keyboard from "./Keyboard";
-import TypingData from "./TypingData";
-import { createRef, useMemo, useRef, useState } from "react";
+import { createRef, useEffect, useMemo, useRef, useState } from "react";
+import TypingResultModal from "./TypingResultModal";
+import LoadingPage from "../LoadingPage/LoadingPage";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function TypingTestPage() {
-  let [words] = useState(() => {
-    return randomWords({ exactly: 1000 });
-  });
+  let [words, setWords] = useState(randomWords({ min: 300, max: 1000 }));
   let inputRef = useRef(null);
   let typingBox = useRef(null);
   let [keypress, setKeyPress] = useState();
@@ -16,7 +16,10 @@ export default function TypingTestPage() {
   let [charIndex, setCharIndex] = useState(0);
   let [correctWordCount, setCorrectWordCount] = useState(0);
   let [correctCharCount, setCorrectCharCount] = useState(0);
-  let [testState, setTestState] = useState("start");
+  let [showResultModal, setShowResultModal] = useState(false);
+  let [testState, setTestState] = useState("");
+  let [loading, setLoading] = useState(false);
+  let [count, setCount] = useState(60);
 
   let wordRefArr = useMemo(() => {
     return Array(words.length)
@@ -26,7 +29,7 @@ export default function TypingTestPage() {
       });
   }, [words]);
 
-  let divElements = useMemo(() => {
+  let wordData = useMemo(() => {
     return words.map((word, index) => {
       return (
         <div key={index} className="flex" ref={wordRefArr[index]}>
@@ -43,7 +46,7 @@ export default function TypingTestPage() {
         </div>
       );
     });
-  }, [wordRefArr, words]);
+  }, [words, wordRefArr]);
 
   function handleType(e) {
     if (
@@ -126,7 +129,7 @@ export default function TypingTestPage() {
       }
 
       if (e.keyCode >= 65 && e.keyCode <= 90) {
-        if (testState === "start") setTestState("end");
+        if (testState === "") setTestState("start");
 
         if (charIndex == charNodes.length) {
           typingBox.current.scrollBy(40, 0);
@@ -182,6 +185,13 @@ export default function TypingTestPage() {
     }
   }
 
+  useEffect(() => {
+    typingBox.current.childNodes[0].scrollIntoView({
+      block: "center",
+      inline: "center",
+    });
+  }, [words]);
+
   function centerWord() {
     wordRefArr[wordIndex + 1].current.scrollIntoView({
       block: "center",
@@ -196,7 +206,33 @@ export default function TypingTestPage() {
     );
   }
 
-  if (testState == "retest") location.reload();
+  function start() {
+    setTimeout(() => {
+      if (count != 0) setCount(count - 1);
+      else return;
+    }, 1000);
+  }
+
+  function stop() {
+    setCount(60);
+  }
+
+  if (testState === "end") {
+    setLoading(true);
+    setTestState("result");
+    setTimeout(() => {
+      setLoading(false);
+      setShowResultModal(true);
+    }, 3000);
+  }
+
+  if (count == 0) {
+    setTestState("end");
+  }
+
+  if (testState == "end") stop();
+
+  if (testState === "start") start();
 
   return (
     <main className="bg-bgColor w-full h-screen flex flex-col">
@@ -208,25 +244,83 @@ export default function TypingTestPage() {
       />
       <Navbar />
       <div className="p-4 text-textColor text-xl leading-normal tracking-wider flex flex-1 flex-col items-center justify-between">
-        <TypingData
-          correctWordCount={correctWordCount}
-          correctCharCount={correctCharCount}
-          attemptedWords={wordIndex}
-          testState={testState}
-          setTestState={setTestState}
-        />
-        <div
-          ref={typingBox}
-          onClick={handleTypeBoxClick}
-          className="cursor-text text-5xl p-8 w-[80%] pl-[40%] flex gap-[40px] items-center justify-start textbox border-[1px] border-thematicColor bg-black/70 text-white overflow-y-scroll rounded-2xl shadow-lg"
-        >
-          {divElements}
+        <div className="flex justify-between w-[60%] gap-8">
+          <div className="w-[30%] rounded-xl bg-thematicColor text-textColor flex flex-col gap-2 pt-2 justify-between items-center shadow-lg">
+            <div
+              id="timer"
+              className="text-8xl flex-1 flex justify-center items-center w-full"
+            >
+              {count}
+            </div>
+
+            <div className="text-sm h-max">
+              {testState === "" ? "Test Time" : "seconds remaining"}
+            </div>
+
+            <progress
+              className="h-[10px] w-full"
+              value={count}
+              max="60"
+            ></progress>
+          </div>
+          <div className="flex gap-8 flex-1 justify-evenly">
+            <div className="w-[30%] rounded-xl bg-thematicColor text-textColor flex flex-col gap-2 justify-center items-center p-2 shadow-lg">
+              <div className="text-8xl text-mono"> {correctWordCount}</div>
+              <div className="text-sm"> words/min</div>
+            </div>
+            <div className="w-[30%] rounded-xl bg-thematicColor text-textColor flex flex-col gap-2 justify-center items-center p-2 shadow-lg">
+              <div className="text-8xl text-mono"> {correctCharCount}</div>
+              <div className="text-sm">chars/min</div>
+            </div>
+            <div className="w-[30%] rounded-xl bg-thematicColor text-textColor flex flex-col gap-2 justify-center items-center p-2 shadow-lg">
+              <div className="text-8xl text-mono">
+                {correctWordCount != 0
+                  ? Math.round((correctWordCount / wordIndex) * 100)
+                  : 0}
+              </div>
+              <div className="text-sm">% accuracy</div>
+            </div>
+          </div>
         </div>
+
+        {testState === "result" ? (
+          <div className="text-5xl p-16 w-[80%] h-[100px] flex items-center justify-center bg-black/70 text-white rounded-2xl shadow-lg scrollbar">
+            Test Over
+          </div>
+        ) : (
+          <div
+            onClick={handleTypeBoxClick}
+            ref={typingBox}
+            id="textbox"
+            className="cursor-text text-5xl p-8 w-[80%] pl-[40%] flex gap-[40px] items-center justify-start  border-[1px] border-thematicColor bg-black/70 text-white overflow-x-scroll rounded-2xl shadow-lg scrollbar"
+          >
+            {wordData}
+          </div>
+        )}
+
         <div className="text-sm text-textColor tracking-widest">
           Just click above and start typing
         </div>
+
         <Keyboard keypress={keypress} />
       </div>
+      {showResultModal && (
+        <TypingResultModal
+          correctWordCount={correctWordCount}
+          setCorrectWordCount={setCorrectWordCount}
+          correctCharCount={correctCharCount}
+          setCorrectCharCount={setCorrectCharCount}
+          attemptedWords={wordIndex}
+          setWordIndex={setWordIndex}
+          setCharIndex={setCharIndex}
+          setTestState={setTestState}
+          showResultModal={showResultModal}
+          setShowResultModal={setShowResultModal}
+          setWords={setWords}
+          typingBox={typingBox}
+        />
+      )}
+      {loading && <LoadingPage />}
     </main>
   );
 }
