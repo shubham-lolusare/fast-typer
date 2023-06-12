50732f20035309841c51dfb42c0eb4c153991efb
Shubham Lolusare <shubhamlolusare@gmail.com>
2023-06-12 22:29:21 +0530

    content added

--- src/components/TypingTestPage/TypingTestPage.jsx
+++ src/components/TypingTestPage/TypingTestPage.jsx
@@ -49,60 +49,62 @@ export default function TypingTestPage() {
         <Navbar />
 
         {/* based on the testMode state the components will be displayed  */}
-        {!mobileMode && testMode == "none" ? (
-          <section className="p-4 text-textColor text-xl leading-normal tracking-wider flex flex-1 items-center justify-center">
-            <section className="w-[40%] p-4 border border-thematicColor rounded-2xl flex flex-col gap-6 shadow-lg md:w-[80%] mobile:w-full">
-              <header className="text-center text-4xl font-bold mobile:text-2xl xs:text-xl">
-                Select the test mode
-              </header>
+        {!mobileMode ? (
+          testMode == "none" && (
+            <section className="p-4 text-textColor text-xl leading-normal tracking-wider flex flex-1 items-center justify-center">
+              <section className="w-[40%] p-4 border border-thematicColor rounded-2xl flex flex-col gap-6 shadow-lg md:w-[80%] mobile:w-full">
+                <header className="text-center text-4xl font-bold mobile:text-2xl xs:text-xl">
+                  Select the test mode
+                </header>
 
-              {/* mode buttons */}
-              <article className="w-full flex gap-4 mobile:flex-col">
-                <button
-                  onClick={() => {
-                    setTestMode("time");
-                  }}
-                  className="p-4 text-2xl font-bold tracking-wide flex-1 bg-thematicColor/90 rounded-lg shadow-lg hover:bg-thematicColor cursor-pointer mobile:text-lg mobile:p-2 xs:text-base"
-                >
-                  Time Mode
-                </button>
-                <button
-                  onClick={() => {
-                    setTestMode("word");
-                  }}
-                  className="p-4 text-2xl font-bold tracking-wide flex-1 bg-thematicColor/90 rounded-lg shadow-lg hover:bg-thematicColor cursor-pointer mobile:text-lg mobile:p-2 xs:text-base"
-                >
-                  Word Mode
-                </button>
-              </article>
+                {/* mode buttons */}
+                <article className="w-full flex gap-4 mobile:flex-col">
+                  <button
+                    onClick={() => {
+                      setTestMode("time");
+                    }}
+                    className="p-4 text-2xl font-bold tracking-wide flex-1 bg-thematicColor/90 rounded-lg shadow-lg hover:bg-thematicColor cursor-pointer mobile:text-lg mobile:p-2 xs:text-base"
+                  >
+                    Time Mode
+                  </button>
+                  <button
+                    onClick={() => {
+                      setTestMode("word");
+                    }}
+                    className="p-4 text-2xl font-bold tracking-wide flex-1 bg-thematicColor/90 rounded-lg shadow-lg hover:bg-thematicColor cursor-pointer mobile:text-lg mobile:p-2 xs:text-base"
+                  >
+                    Word Mode
+                  </button>
+                </article>
 
-              {/* description box */}
-              <article>
-                <h1 className="font-bold text-xl mb-2 mobile:text-lg xs:text-base">
-                  Description:
-                </h1>
-                <ul className="list-decimal list-outside text-base pl-5 mobile:text-sm xs:text-[13px]">
-                  <li className="mb-1">
-                    Time mode lets you take the test within a given time slot
-                    and the Words per minute (WPM) will be calculated according
-                    to the number of words typed by you within the given time
-                    slot. While giving test in this mode, you have to select the
-                    time slot given on test page or by default 1min time slot
-                    will be selected.
-                  </li>
-                  <li>
-                    In word mode, you have to select the number of words you
-                    want to type and the WPM will be calculated by the time
-                    taken by you to type the given words or by default, 20 words
-                    will be selected.
-                  </li>
-                </ul>
-              </article>
+                {/* description box */}
+                <article>
+                  <h1 className="font-bold text-xl mb-2 mobile:text-lg xs:text-base">
+                    Description:
+                  </h1>
+                  <ul className="list-decimal list-outside text-base pl-5 mobile:text-sm xs:text-[13px]">
+                    <li className="mb-1">
+                      Time mode lets you take the test within a given time slot
+                      and the Words per minute (WPM) will be calculated
+                      according to the number of words typed by you within the
+                      given time slot. While giving test in this mode, you have
+                      to select the time slot given on test page or by default
+                      1min time slot will be selected.
+                    </li>
+                    <li>
+                      In word mode, you have to select the number of words you
+                      want to type and the WPM will be calculated by the time
+                      taken by you to type the given words or by default, 20
+                      words will be selected.
+                    </li>
+                  </ul>
+                </article>
+              </section>
+              <footer className="fixed bottom-0 rounded-t-xl p-2 bg-thematicColor text-textColor shadow-lg flex justify-center items-center gap-2">
+                <FooterTabs />
+              </footer>
             </section>
-            <footer className="fixed bottom-0 rounded-t-xl p-2 bg-thematicColor text-textColor shadow-lg flex justify-center items-center gap-2">
-              <FooterTabs />
-            </footer>
-          </section>
+          )
         ) : (
           <section className="w-full h-full p-2 text-textColor text-xl tracking-wider flex items-center justify-center">
             <div className="w-[90%] h-full flex flex-col justify-center gap-6">
